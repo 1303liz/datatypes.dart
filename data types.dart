42f1d9f616cb63ad5e1c2132b82a5c606b9458f5
1303liz <136015794+1303liz@users.noreply.github.com>
2024-03-01 18:31:40 +0300

    Add files via upload

---
+++ data types.dart
@@ -0,0 +1,45 @@
+//numbers
+
+void main()
+{
+  //numbers stores whole numbers while double stores decimal numbers.
+  int num1=100;
+  double num2=34.5;
+  num sum =num1+num2;
+  //string text data in your program
+String Name = " LIZ";
+String GENDER = "FEMALE"; 
+//boolean holds either true or false valu
+bool isstudent = true;
+//a list which is the ordered collection of the objects
+List<String> counties = ["taita", "mombasa", "nakuru"];
+// maps a dynamic collection that represents a set of values ​as key-value pairs. Keys and values ​in the
+
+Map<String, int> marks = {'Alice': 100,
+'Bob': 80,
+'Charlie': 90,
+};
+//runes  can be defined as an integer used to represent any Unicode code point
+ String runesString = "Runes in Dart: \u{1F600} \u{1F64B} \u{1F680}";
+
+
+  print("num 1 is $num1 ");
+   print("num 2 is $num2 ");
+    print("sum 1 is $sum ");
+    ;
+  print("my name is $Name and MY gender is $GENDER");
+  print("student status:$isstudent");
+  print("Value of counties is $counties");
+print("Value of counties[0] is ${counties[0]}"); 
+print("Value of counries[1] is ${counties[1]}"); 
+print("Value of counties[2] is ${counties[2]}");
+print("marks of the students:$marks"); 
+print(runesString);
+
+ 
+
+}
+
+class string {
+}
+  
